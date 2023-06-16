de178a0ccd9b8f79eae2ed8e09e098b54cdee7f7
Patryk Glinka <patrykglinka2@gmail.com>
2023-06-16 14:22:26 +0200

    Added tilt efect on background image using 'react-parallax-tilt' package, need to consider whether keep it or leave.

--- src/components/layouts/backgroundImgLayout.tsx
+++ src/components/layouts/backgroundImgLayout.tsx
@@ -1,20 +1,34 @@
 import styled from "styled-components";
 import Image from "next/image";
 import backgroundImg from "@/public/backgroundImg.jpg";
+import Tilt from "react-parallax-tilt";
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.background};
   display: flex;
   justify-content: space-between;
   height: 100vh;
+  width: 100vw;
 `;
 
 const StyledImage = styled(Image)`
+  /* width: calc(40% - 1rem);
+  height: calc(90% - 2rem); */
+  width: 100%;
+  height: 100%;
+  /* margin: auto; */
+  border-radius: 1rem;
+  box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.25);
+`;
+
+const StyledTilt = styled(Tilt)`
   width: calc(40% - 1rem);
   height: calc(90% - 2rem);
   margin: auto;
-  border-radius: 1rem;
-  box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    cursor: none;
+  }
 `;
 
 const Container = styled.div`
@@ -30,7 +44,15 @@ export default function BackgroundImgLayout({ children }: childrenProp) {
   return (
     <Wrapper>
       <Container>{children}</Container>
-      <StyledImage src={backgroundImg} alt="campervan on desert" />
+      <StyledTilt
+        tiltReverse={true}
+        tiltMaxAngleX={5}
+        tiltMaxAngleY={5}
+        scale={0.95}
+        transitionSpeed={2000}
+      >
+        <StyledImage src={backgroundImg} alt="campervan on desert" />
+      </StyledTilt>
     </Wrapper>
   );
 }
