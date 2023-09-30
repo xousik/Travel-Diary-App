b3b9000dfd1d5ea07a34003930413d1e19060bd8
Patryk Glinka <patrykglinka2@gmail.com>
2023-09-30 20:56:36 +0200

    Actualy loged in userName is visible in logedIn page header.

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,7 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  console.log(req);
   const data = await req.json();
   console.log(data);
 
