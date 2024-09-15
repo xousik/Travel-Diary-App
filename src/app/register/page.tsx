ba7a7acb246f72c349bd908880fbd08185735926
Patryk Glinka <patrykglinka2@gmail.com>
2024-09-15 19:53:35 +0200

    Add LoginPageSentence also to register page.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,12 +2,14 @@ import { LoginPageWrapper } from "@/src/components/atoms/loginPage/loginPageWrap
 import BackgroundImgLayout from "@/src/components/layouts/backgroundImgLayout/backgroundImgLayout";
 import MainTitle from "@/src/components/atoms/mainTitle";
 import RegisterForm from "@/src/components/organisms/registerForm";
+import LoginPageSentence from "@/src/components/atoms/loginPage/loginPageSentence";
 
 export default function Register() {
   return (
     <BackgroundImgLayout>
       <LoginPageWrapper>
         <MainTitle />
+        <LoginPageSentence />
         <RegisterForm />
       </LoginPageWrapper>
     </BackgroundImgLayout>
